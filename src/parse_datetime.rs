7419a622f9a0d2c5fe6b25fe6a97921e4e1de82b
mld-0 <mld.0@protonmail.com>
2023-05-11 20:30:44 +1000

    'parse_datetimes' with tests

--- src/parse_datetime.rs
+++ src/parse_datetime.rs
@@ -5,6 +5,48 @@
 use chrono::{DateTime, FixedOffset, Local, NaiveDateTime, TimeZone};
 use std::collections::HashMap;
 
+/// Parses a vector of strings into `DateTime<FixedOffset>` objects.
+///
+/// This function iterates over each string in the input vector, attempts to parse it into a `DateTime<FixedOffset>` object,
+/// and stores the successfully parsed DateTime objects in a new vector. 
+///
+/// See `parse_datetime` for formats that can be parsed
+///
+/// # Arguments
+/// * `datetimes_strs` - A vector of strings where each string represents a datetime.
+///
+/// # Returns
+/// * `Some(Vec<DateTime<FixedOffset>>)` if all strings are successfully parsed
+/// * `None` if any of the strings cannot be parsed
+///
+/// # Examples
+/// ```
+/// use datetimescan::parse_datetime::parse_datetimes;
+/// let datetimes = vec![
+///     "2023-05-11T12:00:00+00:00".to_string(),
+///     "2023-05-12T12:00:00+00:00".to_string(),
+///     "2023-05-13T12:00:00+00:00".to_string(),
+/// ];
+/// let parsed = parse_datetimes(&datetimes);
+/// assert!(parsed.is_some());
+/// ```
+pub fn parse_datetimes(datetimes_strs: &Vec<String>) -> Option<Vec<DateTime<FixedOffset>>>
+{
+    log::trace!("parse_datetimes(), datetimes_str=({:?})", datetimes_strs);
+    let mut result = Vec::with_capacity(datetimes_strs.len());
+    for datetime_str in datetimes_strs {
+        let loop_result = parse_datetime(datetime_str);
+        if loop_result.is_none() {
+            log::warn!("parse_datetimes(), failed to parse datetime_str=({}), return None", datetime_str);
+            return None
+        }
+        result.push(loop_result.unwrap());
+    }
+    log::trace!("parse_datetimes(), result=({:?})", result);
+    Some(result)
+}
+
+
 /// Parses a datetime string and returns a `DateTime<FixedOffset>` object.
 ///
 /// The input datetime string can be in various formats, such as RFC3339, with or without
--- tests/parse_datetime.rs
+++ tests/parse_datetime.rs
@@ -1,63 +1,103 @@
-use datetimescan::parse_datetime::parse_datetime;
 
-use chrono::{DateTime, Local, NaiveDateTime, TimeZone};
+mod test_parse_datetimes {
+    use datetimescan::parse_datetime::parse_datetimes;
+    use chrono::{DateTime, FixedOffset};
 
-#[test]
-fn test_parse_datetime_rfc3339() {
-    let datetime = parse_datetime("2023-05-08T18:30:00+02:00").unwrap();
-    assert_eq!(
-        datetime,
-        DateTime::parse_from_rfc3339("2023-05-08T18:30:00+02:00").unwrap()
-    );
+    #[test]
+    fn test_parses_valid_dates() {
+        let valid_dates = vec![
+            "2023-05-11T12:00:00+00:00".to_string(),
+            "2023-05-12T12:00:00+00:00".to_string(),
+            "2023-05-13T12:00:00+00:00".to_string(),
+        ];
+        let expected: Vec<DateTime<FixedOffset>> = valid_dates
+            .iter()
+            .map(|s| DateTime::parse_from_rfc3339(s).unwrap())
+            .collect();
+        let parsed = parse_datetimes(&valid_dates).unwrap();
+        assert_eq!(parsed, expected);
+    }
+
+    #[test]
+    fn test_returns_none_for_invalid_dates() {
+        let invalid_dates = vec![
+            "2023-05-11T12:00:00+00:00".to_string(),
+            "invalid".to_string(),
+        ];
+        let parsed = parse_datetimes(&invalid_dates);
+        assert!(parsed.is_none());
+    }
+
+    #[test]
+    fn test_empty_vector() {
+        let empty_vec: Vec<String> = Vec::new();
+        let parsed = parse_datetimes(&empty_vec);
+        assert_eq!(parsed, Some(Vec::new()));
+    }
 }
 
-#[test]
-fn test_parse_datetime_without_t_separator() {
-    let datetime = parse_datetime("2023-05-08 18:30:00").unwrap();
-    let naive_datetime = NaiveDateTime::parse_from_str("2023-05-08 18:30:00", "%Y-%m-%d %H:%M:%S").unwrap();
-    let local_offset = *Local::now().offset();
-    assert_eq!(
-        datetime,
-        local_offset.from_local_datetime(&naive_datetime).unwrap()
-    );
-}
 
-#[test]
-fn test_parse_datetime_without_timezone_offset() {
-    let datetime = parse_datetime("2023-05-08T18:30:00").unwrap();
-    let naive_datetime = NaiveDateTime::parse_from_str("2023-05-08T18:30:00", "%Y-%m-%dT%H:%M:%S").unwrap();
-    let local_offset = *Local::now().offset();
-    assert_eq!(
-        datetime,
-        local_offset.from_local_datetime(&naive_datetime).unwrap()
-    );
-}
+mod test_parse_datetime {
+    use datetimescan::parse_datetime::parse_datetime;
+    use chrono::{DateTime, Local, NaiveDateTime, TimeZone};
 
-#[test]
-fn test_parse_datetime_with_tzcode_aedt() {
-    let datetime = parse_datetime("2023-05-08T18:30:00AEDT").unwrap();
-    assert_eq!(
-        datetime,
-        DateTime::parse_from_str("2023-05-08T18:30:00+1100", "%Y-%m-%dT%H:%M:%S%z").unwrap()
-    );
-}
+    #[test]
+    fn test_rfc3339() {
+        let datetime = parse_datetime("2023-05-08T18:30:00+02:00").unwrap();
+        assert_eq!(
+            datetime,
+            DateTime::parse_from_rfc3339("2023-05-08T18:30:00+02:00").unwrap()
+        );
+    }
 
-#[test]
-fn test_parse_datetime_with_tzcode_utc() {
-    let datetime = parse_datetime("2023-05-08T18:30:00UTC").unwrap();
-    assert_eq!(
-        datetime,
-        DateTime::parse_from_str("2023-05-08T18:30:00+0000", "%Y-%m-%dT%H:%M:%S%z").unwrap()
-    );
-}
+    #[test]
+    fn test_without_t_separator() {
+        let datetime = parse_datetime("2023-05-08 18:30:00").unwrap();
+        let naive_datetime = NaiveDateTime::parse_from_str("2023-05-08 18:30:00", "%Y-%m-%d %H:%M:%S").unwrap();
+        let local_offset = *Local::now().offset();
+        assert_eq!(
+            datetime,
+            local_offset.from_local_datetime(&naive_datetime).unwrap()
+        );
+    }
 
-#[test]
-fn test_parse_datetime_invalid_input() {
-    assert!(parse_datetime("invalid").is_none());
-}
+    #[test]
+    fn test_without_timezone_offset() {
+        let datetime = parse_datetime("2023-05-08T18:30:00").unwrap();
+        let naive_datetime = NaiveDateTime::parse_from_str("2023-05-08T18:30:00", "%Y-%m-%dT%H:%M:%S").unwrap();
+        let local_offset = *Local::now().offset();
+        assert_eq!(
+            datetime,
+            local_offset.from_local_datetime(&naive_datetime).unwrap()
+        );
+    }
 
-#[test]
-fn test_parse_datetime_empty_input() {
-    assert!(parse_datetime("").is_none());
+    #[test]
+    fn test_with_tzcode_aedt() {
+        let datetime = parse_datetime("2023-05-08T18:30:00AEDT").unwrap();
+        assert_eq!(
+            datetime,
+            DateTime::parse_from_str("2023-05-08T18:30:00+1100", "%Y-%m-%dT%H:%M:%S%z").unwrap()
+        );
+    }
+
+    #[test]
+    fn test_with_tzcode_utc() {
+        let datetime = parse_datetime("2023-05-08T18:30:00UTC").unwrap();
+        assert_eq!(
+            datetime,
+            DateTime::parse_from_str("2023-05-08T18:30:00+0000", "%Y-%m-%dT%H:%M:%S%z").unwrap()
+        );
+    }
+
+    #[test]
+    fn test_invalid_input() {
+        assert!(parse_datetime("invalid").is_none());
+    }
+
+    #[test]
+    fn test_empty_input() {
+        assert!(parse_datetime("").is_none());
+    }
 }
 
