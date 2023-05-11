103eeb474a82e28496ac2f977574bc5ca2213bdc
mld-0 <mld.0@protonmail.com>
2023-05-11 21:01:25 +1000

    implement command: 'deltas', cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@
 //  {{{2
 #![allow(unused)]
 use datetimescan::search_datetimes::search_datetimes;
-use datetimescan::parse_datetime::parse_datetime;
+use datetimescan::parse_datetime::{parse_datetime, parse_datetimes};
 use datetimescan::delta_datetimes::{delta_datetimes, datetime_difference_seconds};
 
+use chrono::{DateTime, FixedOffset};
 use clap::{App, Arg, ArgMatches, SubCommand};
 use std::fs::File;
 use std::io::{self,BufReader};
@@ -57,6 +58,7 @@ fn main()
         .subcommand(
             SubCommand::with_name("deltas")
                 .about("")
+                .arg(allow_negative.clone())
             )
         .subcommand(
             SubCommand::with_name("splits")
@@ -75,61 +77,66 @@ fn main()
 
     log::trace!("main(), matches=({:?})", matches);
     if let Some(scan_matches) = matches.subcommand_matches("scan") {
-        run_scan(&scan_matches);
+        scan(&scan_matches);
     } else if let Some(count_matches) = matches.subcommand_matches("count") {
-        run_count(&count_matches);
+        count(&count_matches);
     } else if let Some(deltas_matches) = matches.subcommand_matches("deltas") {
-        run_deltas(&deltas_matches);
+        deltas(&deltas_matches);
     } else if let Some(splits_matches) = matches.subcommand_matches("splits") {
-        run_splits(&splits_matches);
+        splits(&splits_matches);
     } else if let Some(sum_matches) = matches.subcommand_matches("sum") {
-        run_sum(&sum_matches);
+        sum(&sum_matches);
     } else if let Some(wpm_matches) = matches.subcommand_matches("wpm") {
-        run_wpm(&wpm_matches);
+        wpm(&wpm_matches);
     } else {
         eprintln!("No subcommand was used. Use --help for more information.");
     }
 }
 
-fn run_scan(scan_matches: &ArgMatches)
+fn scan(scan_matches: &ArgMatches)
 {
     let datetimes_and_locations = run_search_datetimes(&scan_matches);
     print_search_datetimes_results(&datetimes_and_locations);
 }
 
-fn run_count(count_matches: &ArgMatches)
+fn count(count_matches: &ArgMatches)
 {
-    let datetimes_and_locations = run_search_datetimes(&count_matches);
     let interval = count_matches.value_of("per").unwrap();
-    unimplemented!();
+    let datetimes_and_locations = run_search_datetimes(&count_matches);
+    unimplemented!("UNIMPLEMENTED");
 }
 
-fn run_deltas(deltas_matches: &ArgMatches)
+fn deltas(deltas_matches: &ArgMatches)
 {
-    let datetimes_and_locations = run_search_datetimes(&deltas_matches);
     let allow_negative = deltas_matches.is_present("allow_negative");
-    unimplemented!();
+    let datetimes_and_locations = run_search_datetimes(&deltas_matches);
+    let datetimes_parsed = run_parse_datetimes(&datetimes_and_locations);
+    let deltas = delta_datetimes(&datetimes_parsed, allow_negative);
+    print_deltas(&deltas);
 }
 
-fn run_splits(splits_matches: &ArgMatches) 
+fn splits(splits_matches: &ArgMatches) 
 {
-    let datetimes_and_locations = run_search_datetimes(&splits_matches);
     let allow_negative = false;
-    unimplemented!();
+    let datetimes_and_locations = run_search_datetimes(&splits_matches);
+    let datetimes_parsed = run_parse_datetimes(&datetimes_and_locations);
+    let deltas = delta_datetimes(&datetimes_parsed, allow_negative);
+    unimplemented!("UNIMPLEMENTED");
 }
 
-fn run_sum(sum_matches: &ArgMatches) 
+fn sum(sum_matches: &ArgMatches) 
 {
-    let datetimes_and_locations = run_search_datetimes(&sum_matches);
     let interval = sum_matches.value_of("per").unwrap();
     let allow_negative = false;
-    unimplemented!();
+    let datetimes_and_locations = run_search_datetimes(&sum_matches);
+    unimplemented!("UNIMPLEMENTED");
 }
 
-fn run_wpm(wpm_matches: &ArgMatches) 
+fn wpm(wpm_matches: &ArgMatches) 
 {
+    let allow_negative = false;
     let datetimes_and_locations = run_search_datetimes(&wpm_matches);
-    unimplemented!();
+    unimplemented!("UNIMPLEMENTED");
 }
 
 fn run_search_datetimes(matches: &ArgMatches) -> Vec<(String, usize, usize)>
@@ -143,11 +150,29 @@ fn run_search_datetimes(matches: &ArgMatches) -> Vec<(String, usize, usize)>
     }
 }
 
+fn run_parse_datetimes(datetimes_and_locations: &Vec<(String, usize, usize)>) -> Vec<DateTime<FixedOffset>>
+{
+
+    let datetimes_strs = datetimes_and_locations.iter().map(|(s, _, _)| s.to_string()).collect();
+    let datetimes_parsed = parse_datetimes(&datetimes_strs);
+    if datetimes_parsed.is_none() {
+        panic!("failed to parse datetimes_strs=({:?})", datetimes_strs);
+    }
+    datetimes_parsed.unwrap()
+}
+
 fn print_search_datetimes_results(datetimes_and_locations: &Vec<(String, usize, usize)>)
 {
     let ofs = "\t".to_string();
     for (datetime, line_number, position) in datetimes_and_locations {
         println!("{}{}{}{}{}", datetime, ofs, line_number, ofs, position);
+    }
+}
+
+fn print_deltas(deltas: &Vec<i64>) 
+{
+    for delta in deltas {
+        println!("{}", delta);
     }
 }
 
