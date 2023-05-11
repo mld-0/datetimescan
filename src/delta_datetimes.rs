90f692e4d89092b84b07facc296af9a07400148c
mld-0 <mld.0@protonmail.com>
2023-05-11 18:23:48 +1000

    'delta_datetimes' with tests

--- src/delta_datetimes.rs
+++ src/delta_datetimes.rs
@@ -4,6 +4,46 @@
 //  {{{2
 use chrono::{DateTime, FixedOffset};
 
+/// Calculates the difference in seconds between consecutive `DateTime` objects.
+///
+/// # Arguments
+/// * `datetimes` - A vector of `DateTime` objects in `FixedOffset` timezone. 
+///     The `DateTime` objects should be in ascending order for meaningful results.
+/// * `allow_negatives` - A boolean value indicating whether negative differences 
+///     should be preserved. If `false`, any negative difference is replaced with `0`.
+///
+/// # Returns
+/// A vector of integers representing the difference in seconds between each consecutive 
+/// pair of `DateTime` objects. The difference is calculated as `datetimes[i] - datetimes[i-1]`.
+///
+/// # Example
+/// ```
+/// use chrono::{DateTime, FixedOffset};
+/// let dt1 = DateTime::parse_from_rfc3339("2023-05-11T00:00:00+00:00").unwrap();
+/// let dt2 = DateTime::parse_from_rfc3339("2023-05-11T00:01:00+00:00").unwrap();
+/// let diff = datetimescan::delta_datetimes::delta_datetimes(vec![dt1, dt2], false);
+/// assert_eq!(diff, vec![60]);
+/// ```
+pub fn delta_datetimes(datetimes: Vec<DateTime<FixedOffset>>, allow_negatives: bool) -> Vec<i64>
+{
+    if datetimes.len() == 0 {
+        return vec![];
+    }
+    let mut result = Vec::with_capacity(datetimes.len() - 1);
+    let mut i = 1;
+    while i < datetimes.len() {
+        let delta = datetime_difference_seconds(datetimes[i-1], datetimes[i]);
+        if allow_negatives == false && delta < 0 {
+            result.push(0);
+        } else {
+            result.push(delta);
+        }
+        i += 1;
+    }
+    result
+}
+
+
 /// Calculates the signed difference between two DateTime<FixedOffset> values in seconds.
 ///
 /// This function takes two DateTime<FixedOffset> values as input and calculates the signed difference
@@ -15,10 +55,8 @@ use chrono::{DateTime, FixedOffset};
 /// ```
 /// use chrono::{DateTime, FixedOffset};
 /// use datetimescan::delta_datetimes::datetime_difference_seconds;
-///
 /// let dt1 = DateTime::parse_from_rfc3339("2019-05-01T00:00:00+00:00").unwrap();
 /// let dt2 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00+00:00").unwrap();
-///
 /// assert_eq!(datetime_difference_seconds(dt1, dt2), 126230400);
 /// ```
 ///
@@ -30,6 +68,7 @@ use chrono::{DateTime, FixedOffset};
 /// An `i64` value representing the signed difference between the two DateTime<FixedOffset> values in seconds.
 pub fn datetime_difference_seconds(dt1: DateTime<FixedOffset>, dt2: DateTime<FixedOffset>) -> i64 
 {
+    eprintln!("datetime_difference_seconds, dt1=({}), dt2=({})", dt1, dt2);
     dt2.signed_duration_since(dt1).num_seconds()
 }
 
--- tests/delta_datetimes.rs
+++ tests/delta_datetimes.rs
@@ -1,43 +1,141 @@
-use chrono::DateTime;
-use datetimescan::delta_datetimes::datetime_difference_seconds; 
 
-#[test]
-fn test_same_datetimes() {
-    let dt1 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00+00:00").unwrap();
-    let dt2 = dt1.clone();
+mod test_delta_datetimes {
+    use chrono::DateTime;
+    use datetimescan::delta_datetimes::delta_datetimes; 
 
-    assert_eq!(datetime_difference_seconds(dt1, dt2), 0);
+    #[test]
+    fn test_empty_input() {
+        let input = vec![];
+        let allow_negatives = true;
+        let expected_output = vec![];
+    
+        let result = delta_datetimes(input, allow_negatives);
+        assert_eq!(result, expected_output);
+    }
+    
+    #[test]
+    fn test_single_element() {
+        let input = vec![DateTime::parse_from_rfc3339("2023-05-11T00:00:00+00:00").unwrap()];
+        let allow_negatives = true;
+        let expected_output = vec![];
+    
+        let result = delta_datetimes(input, allow_negatives);
+        assert_eq!(result, expected_output);
+    }
+    
+    #[test]
+    fn test_positive_deltas() {
+        let input = vec![
+            DateTime::parse_from_rfc3339("2023-05-11T00:00:00+00:00").unwrap(),
+            DateTime::parse_from_rfc3339("2023-05-11T00:01:00+00:00").unwrap(),
+            DateTime::parse_from_rfc3339("2023-05-11T00:02:30+00:00").unwrap(),
+        ];
+        let allow_negatives = true;
+        let expected_output = vec![60, 90];
+    
+        let result = delta_datetimes(input, allow_negatives);
+        assert_eq!(result, expected_output);
+    }
+    
+    #[test]
+    fn test_negative_deltas() {
+        let input = vec![
+            DateTime::parse_from_rfc3339("2023-05-11T00:02:30+00:00").unwrap(),
+            DateTime::parse_from_rfc3339("2023-05-11T00:01:00+00:00").unwrap(),
+            DateTime::parse_from_rfc3339("2023-05-11T00:00:00+00:00").unwrap(),
+        ];
+        let allow_negatives = true;
+        let expected_output = vec![-90, -60];
+    
+        let result = delta_datetimes(input, allow_negatives);
+        assert_eq!(result, expected_output);
+    }
+    
+    #[test]
+    fn test_negative_deltas_not_allowed() {
+        let input = vec![
+            DateTime::parse_from_rfc3339("2023-05-11T00:02:30+00:00").unwrap(),
+            DateTime::parse_from_rfc3339("2023-05-11T00:01:00+00:00").unwrap(),
+            DateTime::parse_from_rfc3339("2023-05-11T00:00:00+00:00").unwrap(),
+        ];
+        let allow_negatives = false;
+        let expected_output = vec![0, 0];
+    
+        let result = delta_datetimes(input, allow_negatives);
+        assert_eq!(result, expected_output);
+    }
+    
+    #[test]
+    fn test_mixed_deltas() {
+        let input = vec![
+            DateTime::parse_from_rfc3339("2023-05-11T00:00:00+00:00").unwrap(),
+            DateTime::parse_from_rfc3339("2023-05-11T00:01:00+00:00").unwrap(),
+            DateTime::parse_from_rfc3339("2023-05-11T00:00:30+00:00").unwrap(),
+        ];
+        let allow_negatives = true;
+        let expected_output = vec![60, -30];
+    
+        let result = delta_datetimes(input, allow_negatives);
+        assert_eq!(result, expected_output);
+    }
+    
+    #[test]
+    fn test_mixed_deltas_negatives_not_allowed() {
+        let input = vec![
+            DateTime::parse_from_rfc3339("2023-05-11T00:00:00+00:00").unwrap(),
+            DateTime::parse_from_rfc3339("2023-05-11T00:01:00+00:00").unwrap(),
+            DateTime::parse_from_rfc3339("2023-05-11T00:00:30+00:00").unwrap(),
+        ];
+        let allow_negatives = false;
+        let expected_output = vec![60, 0];
+    
+        let result = delta_datetimes(input, allow_negatives);
+        assert_eq!(result, expected_output);
+    }
 }
 
-#[test]
-fn test_positive_difference() {
-    let dt1 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00+00:00").unwrap();
-    let dt2 = DateTime::parse_from_rfc3339("2023-05-01T01:00:00+00:00").unwrap();
+mod test_difference_seconds {
+    use chrono::DateTime;
+    use datetimescan::delta_datetimes::datetime_difference_seconds; 
 
-    assert_eq!(datetime_difference_seconds(dt1, dt2), 3600);
-}
-
-#[test]
-fn test_negative_difference() {
-    let dt1 = DateTime::parse_from_rfc3339("2023-05-01T01:00:00+00:00").unwrap();
-    let dt2 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00+00:00").unwrap();
-
-    assert_eq!(datetime_difference_seconds(dt1, dt2), -3600);
-}
-
-#[test]
-fn test_different_offsets() {
-    let dt1 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00+03:00").unwrap();
-    let dt2 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00-02:00").unwrap();
-
-    assert_eq!(datetime_difference_seconds(dt1, dt2), 5 * 3600);
-}
-
-#[test]
-fn test_years_appart() {
-    let dt1 = DateTime::parse_from_rfc3339("2019-05-01T00:00:00+00:00").unwrap();
-    let dt2 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00+00:00").unwrap();
-
-    assert_eq!(datetime_difference_seconds(dt1, dt2), 1461 * 24 * 60 * 60);
+    #[test]
+    fn test_same_datetimes() {
+        let dt1 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00+00:00").unwrap();
+        let dt2 = dt1.clone();
+    
+        assert_eq!(datetime_difference_seconds(dt1, dt2), 0);
+    }
+    
+    #[test]
+    fn test_positive_difference() {
+        let dt1 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00+00:00").unwrap();
+        let dt2 = DateTime::parse_from_rfc3339("2023-05-01T01:00:00+00:00").unwrap();
+    
+        assert_eq!(datetime_difference_seconds(dt1, dt2), 3600);
+    }
+    
+    #[test]
+    fn test_negative_difference() {
+        let dt1 = DateTime::parse_from_rfc3339("2023-05-01T01:00:00+00:00").unwrap();
+        let dt2 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00+00:00").unwrap();
+    
+        assert_eq!(datetime_difference_seconds(dt1, dt2), -3600);
+    }
+    
+    #[test]
+    fn test_different_offsets() {
+        let dt1 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00+03:00").unwrap();
+        let dt2 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00-02:00").unwrap();
+    
+        assert_eq!(datetime_difference_seconds(dt1, dt2), 5 * 3600);
+    }
+    
+    #[test]
+    fn test_years_appart() {
+        let dt1 = DateTime::parse_from_rfc3339("2019-05-01T00:00:00+00:00").unwrap();
+        let dt2 = DateTime::parse_from_rfc3339("2023-05-01T00:00:00+00:00").unwrap();
+    
+        assert_eq!(datetime_difference_seconds(dt1, dt2), 1461 * 24 * 60 * 60);
+    }
 }
 
