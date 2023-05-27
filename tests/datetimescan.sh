e3c33771d1c823b66151f2281d8f53ff334958e6
mld-0 <mld.0@protonmail.com>
2023-05-27 18:11:17 +1000

    update output, add `build_debug`

--- tests/datetimescan.sh
+++ tests/datetimescan.sh
@@ -18,7 +18,7 @@ set -o pipefail  # don't hide errors within pipes
 
 #set -x 
 
-microtime() { python3 -c 'import time; print(int(time.time_ns()/1000000))' }
+microtime() { python3 -c 'import time; print(int(time.time_ns()/1000000))'; }
 echoerr() { echo "$@" | sed "s|$HOME|~|g" > /dev/stderr; }
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -64,11 +64,9 @@ if [[ ! -f "$path_testfile_worklog_samples" ]]; then
 fi
 #	}}}
 
-cmd_build=( $bin_cargo build --release  )
-bin_datetimescan="$PROJECT_PATH/target/release/datetimescan"
+bin_datetimescan=""		#	set by 'build_release' / 'build_debug'
 
 flag_print_output=0
-flag_report_pass=1
 flag_exit_on_check_comparison_fail=0
 rust_log_level=''		#	set to 'trace' for full output
 
@@ -87,7 +85,8 @@ main() {
 		export RUST_LOG="$rust_log_level";
 	fi
 	local failures_count=0
-	build_release
+	#build_release
+	build_debug
 	run_tests
 	report_failures
 }
@@ -103,6 +102,9 @@ run_tests() {
 		printf "%s\n" "WARNING, func_name unset, non zsh/bash shell" > /dev/stderr
 	fi
 	#	}}}
+	if [[ ! -x "$bin_datetimescan" ]]; then
+		echoerr "$func_name, failed to find bin_datetimescan=($bin_datetimescan)"
+	fi
 	local startTime=$( microtime )
 	test_scan
 	#test_parse
@@ -121,6 +123,7 @@ run_tests() {
 
 
 test_scan() {
+#	{{{
 	#	funcname: {{{
 	local func_name=""
 	if [[ -n "${ZSH_VERSION:-}" ]]; then 
@@ -134,6 +137,8 @@ test_scan() {
 	local result_str=""
 	local expected_str=""
 	local test_num=1
+
+	echo -n "$func_name: " > /dev/stderr
 
 	test_cmd=( $bin_datetimescan scan )
 	result_str=$( echo "" | ${test_cmd[@]} )
@@ -163,8 +168,9 @@ test_scan() {
 `echo "2023-04-19T22:07:40AEST	1	10 2023-04-19T22:08:16AEST	2	0 2023-04-19T22:11:06AEST	4	10 2024-04-19T22:12:54AEST	6	0 2023-04-19T22:14:15AEST	10	0 2023-04-19T22:14:20AEST	12	6 2023-04-19T22:15:31AEST	13	0 2023-04-19T22:15:58AEST	15	9 2023-04-19T22:16:28AEST	16	0 2023-04-19T22:16:51AEST	18	0 2023-04-19T22:18:19AEST	19	0 2023-04-19T22:19:41AEST	21	6 2023-04-19T22:19:55AEST	22	0 2023-04-19T22:21:07AEST	24	9 2023-04-19T22:20:05AEST	25	6 2023-04-19T22:23:14AEST	26	0 2023-04-19T22:26:27AEST	28	0 2023-04-19T22:26:44AEST	29	0 2023-04-19T22:27:55AEST	31	0 2023-04-19T22:29:20AEST	32	0 2023-04-19T22:29:43AEST	34	9 2023-04-19T22:30:08AEST	35	0 2023-04-19T22:30:35AEST	37	0 2023-04-19T22:31:13AEST	38	0 2023-04-19T22:32:48AEST	39	0 2023-04-19T22:32:56AEST	41	0 2023-04-19T22:33:10AEST	42	10 2023-04-19T22:33:51AEST	43	6 2023-04-19T22:34:40AEST	44	0 2023-04-19T22:35:03AEST	46	6 2023-04-19T22:35:42AEST	47	0 2023-04-19T22:37:19AEST	49	9 2023-04-19T22:38:07AEST	50	0 2023-04-19T22:38:14AEST	52	0 2023-04-19T22:38:40AEST	53	0 2023-04-19T22:39:01AEST	55	0 2023-04-19T22:39:32AEST	56	0 2023-04-19T22:40:00AEST	59	0 2023-04-19T22:40:05AEST	60	0 2023-04-19T22:40:31AEST	61	0 2023-04-19T23:21:37AEST	63	0 2023-04-19T23:21:47AEST	64	0 2023-04-19T23:22:02AEST	67	0 2023-04-19T23:22:36AEST	68	0 2023-04-19T23:23:04AEST	69	0 2023-04-19T23:28:52AEST	71	10 2023-04-19T23:29:19AEST	73	7 2023-04-19T23:29:34AEST	74	0 2023-04-19T23:29:49AEST	76	6 2023-04-19T23:30:35AEST	77	0 2023-04-19T23:31:23AEST	80	0 2023-04-19T23:32:58AEST	81	0 2023-04-19T23:33:18AEST	83	6 2023-04-19T23:34:10AEST	84	0 2023-04-19T23:45:06AEST	86	0 2023-04-19T23:45:13AEST	87	0" | tr ' ' '\n'`
 	assert_result
 
-	echoerr "$func_name, DONE"
+	echoerr ""
 }
+#	}}}
 
 
 test_parse() {
@@ -181,8 +187,9 @@ test_parse() {
 	local result_str=""
 	local expected_str=""
 	local test_num=1
+	echo -n "$func_name: " > /dev/stderr
 	echoerr "$func_name, UNIMPLEMENTED"; exit 2;
-	echoerr "$func_name, DONE"
+	echoerr ""
 }
 
 
@@ -200,8 +207,9 @@ test_convert() {
 	local result_str=""
 	local expected_str=""
 	local test_num=1
+	echo -n "$func_name: " > /dev/stderr
 	echoerr "$func_name, WARNING, UNIMPLEMENTED"; exit 2;
-	echoerr "$func_name, DONE"
+	echoerr ""
 }
 
 
@@ -219,12 +227,14 @@ test_filter() {
 	local result_str=""
 	local expected_str=""
 	local test_num=1
+	echo -n "$func_name: " > /dev/stderr
 	echoerr "$func_name, WARNING, UNIMPLEMENTED"; exit 2;
-	echoerr "$func_name, DONE"
+	echoerr ""
 }
 
 
 test_count() {
+#	{{{
 	#	funcname: {{{
 	local func_name=""
 	if [[ -n "${ZSH_VERSION:-}" ]]; then 
@@ -238,6 +248,8 @@ test_count() {
 	local result_str=""
 	local expected_str=""
 	local test_num=1
+	
+	echo -n "$func_name: " > /dev/stderr
 
 	test_cmd=( $bin_datetimescan count --input "$path_testfile_empty" )
 	result_str=$( ${test_cmd[@]} )
@@ -393,11 +405,13 @@ test_count() {
 2023-05-23: 6"
 	assert_result
 
-	echoerr "$func_name, DONE"
+	echoerr ""
 }
+#	}}}
 
 
 test_deltas() {
+#	{{{
 	#	funcname: {{{
 	local func_name=""
 	if [[ -n "${ZSH_VERSION:-}" ]]; then 
@@ -411,6 +425,8 @@ test_deltas() {
 	local result_str=""
 	local expected_str=""
 	local test_num=1
+
+	echo -n "$func_name: " > /dev/stderr
 
 	test_cmd=( $bin_datetimescan deltas --input "$path_testfile_empty" )
 	result_str=$( ${test_cmd[@]} )
@@ -439,11 +455,13 @@ test_deltas() {
 `echo "36 170 31622508 -31622319 5 71 27 30 23 88 82 14 72 -62 189 193 17 71 85 23 25 27 38 95 8 14 41 49 23 39 97 48 7 26 21 31 28 5 26 2466 10 15 34 28 348 27 15 15 46 48 95 20 52 656 7" | tr ' ' '\n'`
 	assert_result
 
-	echoerr "$func_name, DONE"
+	echoerr ""
 }
+#	}}}
 
 
 test_splits() {
+#	{{{
 	#	funcname: {{{
 	local func_name=""
 	if [[ -n "${ZSH_VERSION:-}" ]]; then 
@@ -457,6 +475,8 @@ test_splits() {
 	local result_str=""
 	local expected_str=""
 	local test_num=1
+
+	echo -n "$func_name: " > /dev/stderr
 
 	test_cmd=( $bin_datetimescan splits --input "$path_testfile_empty" )
 	result_str=$( ${test_cmd[@]} )
@@ -572,11 +592,13 @@ test_splits() {
 "2023-04-19: 2033, 87, 318, 7"
 	assert_result
 
-	echoerr "$func_name, DONE"
+	echoerr ""
 }
+#	}}}
 
 
 test_sum() {
+#	{{{
 	#	funcname: {{{
 	local func_name=""
 	if [[ -n "${ZSH_VERSION:-}" ]]; then 
@@ -590,6 +612,8 @@ test_sum() {
 	local result_str=""
 	local expected_str=""
 	local test_num=1
+
+	echo -n "$func_name: " > /dev/stderr
 
 	test_cmd=( $bin_datetimescan sum --input "$path_testfile_empty" )
 	result_str=$( ${test_cmd[@]} )
@@ -699,8 +723,9 @@ test_sum() {
 "3260"
 	assert_result
 
-	echoerr "$func_name, DONE"
+	echoerr ""
 }
+#	}}}
 
 
 test_wpm() {
@@ -718,7 +743,7 @@ test_wpm() {
 	local expected_str=""
 	local test_num=1
 	echoerr "$func_name, UNIMPLEMENTED"; exit 2;
-	echoerr "$func_name, DONE"
+	echoerr ""
 }
 
 
@@ -734,10 +759,38 @@ build_release() {
 		printf "%s\n" "WARNING, func_name unset, non zsh/bash shell" > /dev/stderr
 	fi
 	#	}}}
+	local cmd_build=( $bin_cargo build --release  )
 	echoerr "PROJECT_PATH=($PROJECT_PATH)"
 	echoerr "cmd_build=(${cmd_build[@]})"
 	cd "$PROJECT_PATH"
 	${cmd_build[@]}
+	bin_datetimescan="$PROJECT_PATH/target/release/datetimescan"
+	if [[ ! -x "$bin_datetimescan" ]]; then
+		echoerr "$func_name, failed to bin_datetimescan=($bin_datetimescan)"
+		exit 2
+	fi
+	echoerr "$func_name, DONE"
+}
+#	}}}
+
+build_debug() {
+#	{{{
+	#	funcname: {{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "WARNING, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+	local cmd_build=( $bin_cargo build )
+	echoerr "PROJECT_PATH=($PROJECT_PATH)"
+	echoerr "cmd_build=(${cmd_build[@]})"
+	cd "$PROJECT_PATH"
+	${cmd_build[@]}
+	bin_datetimescan="$PROJECT_PATH/target/debug/datetimescan"
 	if [[ ! -x "$bin_datetimescan" ]]; then
 		echoerr "$func_name, failed to bin_datetimescan=($bin_datetimescan)"
 		exit 2
@@ -748,26 +801,20 @@ build_release() {
 
 assert_result() {
 #	{{{
+	local nl=$'\n'
 	if [[ $flag_print_output -ne 0 ]]; then
 		print_result
 	fi
 	if [[ ! "$result_str" == "$expected_str" ]]; then
-		echoerr "$func_name, fail: $test_num\n"
+		echoerr "$nl$func_name, fail: $test_num"
 		#	use 'if' to prevent errexit triggering (diff returns rc=1)
-		if diff --color -u <( echo $result_str ) <( echo $expected_str ) > /dev/stderr; then echo "" > /dev/null; fi
-		#	{{{
-		#if diff --color <( echo $result_str ) <( echo $expected_str ); then echo "" > /dev/null; fi
-		#if diff <( echo $result_str ) <( echo $expected_str ); then echo "" > /dev/null; fi
-		#diff --color --suppress-common-lines -y <( echo $result_str ) <( echo $expected_str )
-		#	}}}
+		if diff --color <( echo $result_str ) <( echo $expected_str ) > /dev/stderr; then echo "" > /dev/null; fi
 		failures_count=`perl -E "say $failures_count + 1"`
 		if [[ $flag_exit_on_check_comparison_fail -ne 0 ]]; then
 			exit 2
 		fi
 	else
-		if [[ $flag_report_pass -ne 0 ]]; then
-			echoerr "$func_name, pass: $test_num"
-		fi
+		echo -n "." > /dev/stderr
 	fi
 	test_num=$( perl -E "say $test_num + 1" )
 }
